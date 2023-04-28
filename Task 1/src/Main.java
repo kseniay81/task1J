a6c05058618b90351d45f616a296b05c52347f59
Kseny <shavyrinaks@mail.ru>
2023-04-28 14:40:21 +0500

    Task

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -2,12 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-int amountTicket = 16800;
-int amountOneMiles = 20;
+        int amountTicket = 16800;
+        int amountOneMiles = 20;
         System.out.println("Стоимость билета" + amountTicket + "руб.");
         System.out.println("Бонусная программа - за каждые 20 рублей, потраченные на билет, начисляется 1 миля.");
         int bonusMiles = amountTicket / amountOneMiles;
-        System.out.println("Итоговый бонус" + bonusMiles + "миль." );
+        System.out.println("Итоговый бонус" + bonusMiles + "миль.");
 
     }
 }
